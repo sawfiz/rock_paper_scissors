ca4382ed5dfe7a46d2436264c66073edeb9333a3
sawfiz <dashu.up@gmail.com>
2022-10-08 12:24:54 +0800

    Add comments to js code. Remove asyn functions

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const choicesELs = Array.from(document.querySelectorAll('.choice'));
 const resultEl = document.querySelector('.result');
 const computerScoreEl = document.querySelector('#computer-score');
 const playerScoreEl = document.querySelector('#player-score');
+
 const modalEl = document.querySelector('.modal');
 const modelTextEl = document.querySelector('.modal-text');
 const closeModalEl = document.querySelector('.modal-btn');
@@ -22,42 +23,54 @@ const numberOfGames = 5;
 let playerScore = 0;
 let computerScore = 0;
 
+// Returns computer's choice
 function getComputerChoice() {
   const index = Math.floor(Math.random() * choices.length);
-  choicesELs[index].classList.add('computer-choice');
-  return choices[index];
+  return index;
 }
 
 // eslint-disable-next-line consistent-return
 function playOneRound(playerSelection, computerSelection) {
+  // Tie game
   if (playerSelection === computerSelection) {
     tieEl.play();
     return 'Tie';
   }
+
   if (
+    // Player wins
     (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
     (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
     (playerSelection === 'Paper' && computerSelection === 'Rock')
   ) {
     playerScore += 1;
+    // Raise the player choice
     choicesELs[choices.indexOf(playerSelection)].classList.add('win');
+    // Lower the compuer choice
     choicesELs[choices.indexOf(computerSelection)].classList.add('lose');
+    // Play "you win" sound
     youWinEl.play();
     return `You Win! ${playerSelection} beats ${computerSelection}`;
   }
+
   if (
+    // Computer wins
     (playerSelection === 'Rock' && computerSelection === 'Paper') ||
     (playerSelection === 'Scissors' && computerSelection === 'Rock') ||
     (playerSelection === 'Paper' && computerSelection === 'Scissors')
   ) {
     computerScore += 1;
+    // Raise the computer choice
     choicesELs[choices.indexOf(playerSelection)].classList.add('lose');
+    // Loser the player choice
     choicesELs[choices.indexOf(computerSelection)].classList.add('win');
+    // Play "you lose" sound
     youLoseEl.play();
     return `You Lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
 
+// Reset scores
 function clearScores() {
   playerScore = 0;
   computerScore = 0;
@@ -65,33 +78,41 @@ function clearScores() {
   playerScoreEl.innerText = playerScore;
 }
 
+// Reset display for a new play
 function clearChoices() {
-  choicesELs.forEach((element) =>
-    element.classList.remove('computer-choice', 'player-choice', 'win', 'lose')
+  choicesELs.forEach((choice) =>
+    choice.classList.remove('computer-choice', 'player-choice', 'win', 'lose')
   );
   resultEl.classList.remove('show');
 }
 
-function delay(time) {
-  // eslint-disable-next-line no-promise-executor-return
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-async function game() {
+function game() {
   clearChoices();
-  await delay(100);
 
-  const playerSelection = this.id;
-  choicesELs[choices.indexOf(playerSelection)].classList.add('player-choice');
+  // 'this' is the choice element clicked on.
+  // 'id' is defined for the element in HTML, e.g., 'rock', 'paper', 'scissors'
+  // playerSelection is the index of the 'id' in the choices array, 0, 1, 2
+  const playerSelection = choices.indexOf(this.id);
+
+  // Add aqua border to player's choice
+  choicesELs[playerSelection].classList.add('player-choice');
 
   const computerSelection = getComputerChoice();
-  resultEl.innerText = playOneRound(playerSelection, computerSelection);
+  // Add coral border to computer's choice
+  choicesELs[computerSelection].classList.add('computer-choice');
+
+  // Show the result of the play
+  resultEl.innerText = playOneRound(
+    choices[playerSelection],
+    choices[computerSelection]
+  );
   resultEl.classList.add('show');
 
+  // Update the scores
   computerScoreEl.innerText = computerScore;
   playerScoreEl.innerText = playerScore;
 
-  await delay(100);
+  // Check game score
   if (playerScore >= numberOfGames) {
     modalEl.showModal();
     modelTextEl.innerText = 'You win!';
@@ -102,14 +123,18 @@ async function game() {
   }
 }
 
-choicesELs.forEach((element) =>
-  element.addEventListener('mouseout', clearChoices)
-);
-
+// Capture the click on choice elements and call the game function
 choicesELs.forEach((choice) =>
   choice.addEventListener('click', game, { capture: true })
 );
 
+// Only clear choices colors and positions when the players mouseout
+// This allows time for the user to understand the result of the play
+choicesELs.forEach((choice) =>
+  choice.addEventListener('mouseout', clearChoices)
+);
+
+// Modal 'Restart' button
 closeModalEl.addEventListener('click', () => {
   modalEl.close();
   clearScores();
