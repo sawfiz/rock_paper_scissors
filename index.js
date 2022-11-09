d2f70624188663fdc688d1783176eb68b60d7197
sawfiz <dashu.up@gmail.com>
2022-11-09 09:25:46 +0800

    Rewrite index.js, console version only
    Why
    - After learning about the MVC model, I relized the original version of the project was full of mixed logic and DOM manipulation
    - After code review of Shuya's code for this project, I realized I used many global variables, and did not create the correct functions
    
    What
    - Rewrite the entire index.js.
    - This version is the console version only.

--- index.js
+++ index.js
@@ -1,142 +1,57 @@
-/* eslint-disable operator-linebreak */
-/* eslint-disable implicit-arrow-linebreak */
-/* eslint-disable function-paren-newline */
-/* eslint-disable comma-dangle */
-// Project: Rock Paper Scissors
-// https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
+const choices = ['rock', 'paper', 'scissors'];
+const gamesToPlay = 5;
 
-const choicesELs = Array.from(document.querySelectorAll('.choice'));
-const resultEl = document.querySelector('.result');
-const computerScoreEl = document.querySelector('#computer-score');
-const playerScoreEl = document.querySelector('#player-score');
-
-const modalEl = document.querySelector('.modal');
-const modelTextEl = document.querySelector('.modal-text');
-const closeModalEl = document.querySelector('.modal-btn');
-
-const youWinEl = new Audio('sounds/youwin.mp3');
-const youLoseEl = new Audio('sounds/youlose.mp3');
-const tieEl = new Audio('sounds/tom.mp3');
-
-const choices = ['Rock', 'Paper', 'Scissors'];
-const numberOfGames = 5;
-let playerScore = 0;
-let computerScore = 0;
-
-// Returns computer's choice
 function getComputerChoice() {
-  const index = Math.floor(Math.random() * choices.length);
-  return index;
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-// eslint-disable-next-line consistent-return
-function playOneRound(playerSelection, computerSelection) {
+function getPlayerChoice() {
+  return prompt('rock, paper, scissors');
+}
+
+function playRound() {
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+
+  console.log('Player Choice: ', playerChoice);
+  console.log('Computer Choice:', computerChoice);
+
   // Tie game
-  if (playerSelection === computerSelection) {
-    tieEl.play();
-    return 'Tie';
+  if (playerChoice === computerChoice) {
+    console.log('Tie!');
+    return 'tie';
   }
-
+  // Player wins
   if (
-    // Player wins
-    (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-    (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
-    (playerSelection === 'Paper' && computerSelection === 'Rock')
+    (computerChoice === 'rock' && playerChoice === 'paper') ||
+    (computerChoice === 'scissors' && playerChoice === 'rock') ||
+    (computerChoice === 'paper' && playerChoice === 'scissors')
   ) {
-    playerScore += 1;
-    // Raise the player choice
-    choicesELs[choices.indexOf(playerSelection)].classList.add('win');
-    // Lower the compuer choice
-    choicesELs[choices.indexOf(computerSelection)].classList.add('lose');
-    // Play "you win" sound
-    youWinEl.play();
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    console.log('Player wins!');
+    return 'player';
   }
-
-  if (
-    // Computer wins
-    (playerSelection === 'Rock' && computerSelection === 'Paper') ||
-    (playerSelection === 'Scissors' && computerSelection === 'Rock') ||
-    (playerSelection === 'Paper' && computerSelection === 'Scissors')
-  ) {
-    computerScore += 1;
-    // Raise the computer choice
-    choicesELs[choices.indexOf(playerSelection)].classList.add('lose');
-    // Loser the player choice
-    choicesELs[choices.indexOf(computerSelection)].classList.add('win');
-    // Play "you lose" sound
-    youLoseEl.play();
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
-  }
-}
-
-// Reset scores
-function clearScores() {
-  playerScore = 0;
-  computerScore = 0;
-  computerScoreEl.innerText = computerScore;
-  playerScoreEl.innerText = playerScore;
-}
-
-// Reset display for a new play
-function clearChoices() {
-  choicesELs.forEach((choice) =>
-    choice.classList.remove('computer-choice', 'player-choice', 'win', 'lose')
-  );
-  resultEl.classList.remove('show');
+  // Computer wins
+  console.log('Computer wins!');
+  return 'computer';
 }
 
 function game() {
-  clearChoices();
+  let playerScore = 0;
+  let computerScore = 0;
+  let ties = 0;
+  let gamesPlayed = 0;
 
-  // 'this' is the choice element clicked on.
-  // 'id' is defined for the element in HTML, e.g., 'rock', 'paper', 'scissors'
-  // playerSelection is the index of the 'id' in the choices array, 0, 1, 2
-  const playerSelection = choices.indexOf(this.id);
+  while (gamesPlayed < gamesToPlay) {
+    gamesPlayed += 1;
+    console.log('Game', gamesPlayed);
 
-  // Add aqua border to player's choice
-  choicesELs[playerSelection].classList.add('player-choice');
-
-  const computerSelection = getComputerChoice();
-  // Add coral border to computer's choice
-  choicesELs[computerSelection].classList.add('computer-choice');
-
-  // Show the result of the play
-  resultEl.innerText = playOneRound(
-    choices[playerSelection],
-    choices[computerSelection]
-  );
-  resultEl.classList.add('show');
-
-  // Update the scores
-  computerScoreEl.innerText = computerScore;
-  playerScoreEl.innerText = playerScore;
-
-  // Check game score
-  if (playerScore >= numberOfGames) {
-    modalEl.showModal();
-    modelTextEl.innerText = 'You win!';
+    const winner = playRound();
+    ties += winner === 'tie';
+    playerScore += winner === 'player';
+    computerScore += winner === 'computer';
   }
-  if (computerScore >= numberOfGames) {
-    modalEl.showModal();
-    modelTextEl.innerText = 'You lose!';
-  }
+
+  console.log(`Game Over.  Player: ${playerScore}  Computer: ${computerScore}  Ties: ${ties}`);
 }
 
-// Capture the click on choice elements and call the game function
-choicesELs.forEach((choice) =>
-  choice.addEventListener('click', game)
-  // choice.addEventListener('click', game, { capture: true })
-);
-
-// Only clear choices colors and positions when the players mouseout
-// This allows time for the user to understand the result of the play
-choicesELs.forEach((choice) =>
-  choice.addEventListener('mouseout', clearChoices)
-);
-
-// Modal 'Restart' button
-closeModalEl.addEventListener('click', () => {
-  modalEl.close();
-  clearScores();
-});
+game();
