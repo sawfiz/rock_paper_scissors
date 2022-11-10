50d211006bb24f7097e016c12954ccdd485d3b63
sawfiz <dashu.up@gmail.com>
2022-11-10 11:11:36 +0800

    Rewrite js code using promises

--- index.js
+++ index.js
@@ -1,84 +1,98 @@
 const gamesToPlay = 5;
-let playerScore = 0;
-let computerScore = 0;
-let ties = 0;
-let gamesPlayed = 0;
-
-function resetScores() {
-  playerScore = 0;
-  computerScore = 0;
-  ties = 0;
-  gamesPlayed = 0;
-}
 
 function getComputerChoice() {
   const choices = ['rock', 'paper', 'scissors'];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function getPlayerChoice(callback) {
+function getPlayerChoice() {
   const rockEl = document.getElementById('rock');
   const paperEl = document.getElementById('paper');
   const scissorsEl = document.getElementById('scissors');
 
-  rockEl.addEventListener('click', () => {
-    callback('rock');
+  return new Promise((resolve) => {
+    rockEl.addEventListener('click', () => {
+      resolve('rock');
+    });
+    paperEl.addEventListener('click', () => {
+      resolve('paper');
+    });
+    scissorsEl.addEventListener('click', () => {
+      resolve('scissors');
+    });
+    //   return prompt('rock, paper, scissors');
   });
-  paperEl.addEventListener('click', () => {
-    callback('paper');
-  });
-  scissorsEl.addEventListener('click', () => {
-    callback('scissors');
-  });
-
-  //   return prompt('rock, paper, scissors');
 }
 
-function tallyScore(winner) {
-  ties += winner === 'tie';
-  playerScore += winner === 'player';
-  computerScore += winner === 'computer';
-
-  gamesPlayed += 1;
-  console.log(ties, playerScore, computerScore);
-  if (gamesPlayed >= gamesToPlay) {
-    console.log(
-      `Game Over.  Player: ${playerScore}  Computer: ${computerScore}  Ties: ${ties}`
-    );
-    alert('Play again?');
-    resetScores();
-  }
-}
-
-function playRound(playerChoice) {
+function playRound() {
   let winner;
-  // const playerChoice = getPlayerChoice();
-  const computerChoice = getComputerChoice();
+  return new Promise((resolve) => {
+    getPlayerChoice().then((playerChoice) => {
+      console.log(playerChoice);
+      const computerChoice = getComputerChoice();
 
-  console.log('Player Choice: ', playerChoice);
-  console.log('Computer Choice:', computerChoice);
+      console.log('Player Choice: ', playerChoice);
+      console.log('Computer Choice:', computerChoice);
 
-  // Tie game
-  if (playerChoice === computerChoice) {
-    console.log('Tie!');
-    winner = 'rock';
-  } else if (
-    (computerChoice === 'rock' && playerChoice === 'paper') ||
-    (computerChoice === 'scissors' && playerChoice === 'rock') ||
-    (computerChoice === 'paper' && playerChoice === 'scissors')
-  ) {
-    console.log('Player wins!');
-    winner = 'player';
-  } else {
-    // Computer wins
-    console.log('Computer wins!');
-    winner = 'computer';
+      // Tie game
+      if (playerChoice === computerChoice) {
+        console.log('Tie!');
+        winner = 'rock';
+      } else if (
+        (computerChoice === 'rock' && playerChoice === 'paper') ||
+        (computerChoice === 'scissors' && playerChoice === 'rock') ||
+        (computerChoice === 'paper' && playerChoice === 'scissors')
+      ) {
+        console.log('Player wins!');
+        winner = 'player';
+      } else {
+        // Computer wins
+        console.log('Computer wins!');
+        winner = 'computer';
+      }
+      resolve(winner);
+    });
+  });
+}
+
+async function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let ties = 0;
+  let gamesPlayed = 0;
+
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise(async (resolve) => {
+    while (gamesPlayed < gamesToPlay) {
+      // eslint-disable-next-line no-await-in-loop, no-loop-func
+      await playRound().then((winner) => {
+        console.log('winner is', winner);
+        ties += winner === 'tie';
+        playerScore += winner === 'player';
+        computerScore += winner === 'computer';
+
+        gamesPlayed += 1;
+        console.log(ties, playerScore, computerScore);
+        if (gamesPlayed >= gamesToPlay) {
+          console.log(
+            `Game Over.  Player: ${playerScore}  Computer: ${computerScore}  Ties: ${ties}`
+          );
+          resolve();
+        }
+      });
+    }
+  });
+}
+
+async function gameMaster() {
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    // eslint-disable-next-line no-await-in-loop
+    await game().then(() => {
+      // eslint-disable-next-line no-alert
+      alert('Play again?');
+    });
   }
-  tallyScore(winner);
 }
 
-function game() {
-  getPlayerChoice(playRound);
-}
-
-game();
+gameMaster();
